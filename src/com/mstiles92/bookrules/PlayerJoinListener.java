0c34b0d21b8c4e9c9fc045a9c1abefac1e7e4cb6
mstiles92 <mstiles92@gmail.com>
2013-01-07 17:18:31 -0500

    Add licensing information to all source files

--- src/com/mstiles92/bookrules/PlayerJoinListener.java
+++ src/com/mstiles92/bookrules/PlayerJoinListener.java
@@ -1,3 +1,26 @@
+/**
+ * This document is a part of the source code and related artifacts for
+ * BookRules, an open source Bukkit plugin for automatically distributing
+ * written books to new players.
+ *
+ * http://dev.bukkit.org/server-mods/bookrules/
+ * http://github.com/mstiles92/BookRules
+ *
+ * Copyright © 2013 Matthew Stiles (mstiles92)
+ *
+ * Licensed under the Common Development and Distribution License Version 1.0
+ * You may not use this file except in compliance with this License.
+ *
+ * You may obtain a copy of the CDDL-1.0 License at
+ * http://opensource.org/licenses/CDDL-1.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the license.
+ */
+
 package com.mstiles92.bookrules;
 
 import org.bukkit.ChatColor;
